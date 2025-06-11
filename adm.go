1fea98d2d03e01e090751e00e7b47c620352292f
20213285 <n.samimi.dehkordi@tue.nl>
2025-06-11 15:16:11 +0200

    handling fpt

--- adm.go
+++ adm.go
@@ -10,20 +10,23 @@ const defaultSequence = "0"
 
 // configurations passed to the plugin
 type Config struct {
-	Sequence string `json:"sequence,omitempty"`
+	Sequence     string `json:"sequence,omitempty"`
+	UpdatePrefix string `json:"updatePrefix,omitempty"`
 }
 
 func CreateConfig() *Config {
 	return &Config{
-		Sequence: defaultSequence,
+		Sequence:     defaultSequence,
+		UpdatePrefix: "/web-app1",
 	}
 }
 
 // necessary components of a traefik plugin
 type MKAdm struct {
-	next     http.Handler
-	sequence string
-	name     string
+	next         http.Handler
+	sequence     string
+	name         string
+	updatePrefix string // add this
 }
 
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
@@ -32,14 +35,25 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 	}
 
 	return &MKAdm{
-		next:     next,
-		sequence: config.Sequence,
-		name:     name,
+		next:         next,
+		sequence:     config.Sequence,
+		name:         name,
+		updatePrefix: config.UpdatePrefix,
 	}, nil
 
 }
 
 func (m *MKAdm) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path == m.updatePrefix+"/admission-update" {
+		// You can rotate, reset, or log here
+		m.sequence, _ = rotateSequence(m.sequence)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Admission sequence updated\n"))
+		return
+	}
+
 	decision := "0"
 	m.sequence, decision = rotateSequence(m.sequence)
 	//header injection to the backend service
