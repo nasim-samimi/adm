0aa8ebe6784db005b91c8b96efd6284d8f161c79
20213285 <n.samimi.dehkordi@tue.nl>
2025-06-11 18:16:50 +0200

    handling fpt

--- adm.go
+++ adm.go
@@ -44,40 +44,35 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 
 }
 
-// func (m *MKAdm) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-// 	if r.URL.Path == m.updatePrefix+"/admission-update" {
-// 		// You can rotate, reset, or log here
-// 		m.sequence, _ = rotateSequence(m.sequence)
-
-// 		w.WriteHeader(http.StatusOK)
-// 		w.Write([]byte("Admission sequence updated\n"))
-// 		return
-// 	}
-
-// 	log.Println("Plugin hit! Path:", r.URL.Path)
-
-// 	decision := "0"
-// 	m.sequence, decision = rotateSequence(m.sequence)
-// 	//header injection to the backend service
-// 	// r.Header.Set("X-Request-ID", uid)
-
-// 	//header injection to the client response
-// 	w.Header().Add("Sequence", decision)
-// 	if decision == "0" {
-// 		w.WriteHeader(http.StatusForbidden)
-// 		return
-// 	}
-
-// 	// w.Write([]byte("Hello, World!"))
-// 	log.Println("Request allowed, forwarding to next handler.")
-// 	m.next.ServeHTTP(w, r)
-// }
-
 func (m *MKAdm) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("[adm-plugin] Received request: %s", r.URL.Path)
+
+	// if r.URL.Path == m.updatePrefix+"/admission-update" {
+	// 	// You can rotate, reset, or log here
+	// 	m.sequence, _ = rotateSequence(m.sequence)
+
+	// 	w.WriteHeader(http.StatusOK)
+	// 	w.Write([]byte("Admission sequence updated\n"))
+	// 	return
+	// }
+
+	// log.Println("Plugin hit! Path:", r.URL.Path)
+
+	// decision := "0"
+	// m.sequence, decision = rotateSequence(m.sequence)
+
+	//header injection to the backend service
+	r.Header.Set("X-Request-ID", "10101010")
+
+	//header injection to the client response
+	// w.Header().Add("Sequence", decision)
+	// if decision == "0" {
+	// 	w.WriteHeader(http.StatusForbidden)
+	// 	return
+	// }
+
+	w.Write([]byte("Hello, World!"))
+	log.Println("Request allowed, forwarding to next handler.")
 	m.next.ServeHTTP(w, r)
-	// panic("Testing plugin invocation")
 }
 
 // func rotateSequence(s string) (string, string) {
